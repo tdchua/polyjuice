e223ab3d09e388d8d6bcdc06622d93c40e51a4df
Timothy Joshua Chua <timothy.d.chua@gmail.com>
2020-03-31 08:47:24 +0800

    Updated documentation, added comments, changed training parameters

--- TM/faceswap.py
+++ TM/faceswap.py
@@ -1,3 +1,6 @@
+#Created by Timothy Joshua Dy Chua
+#This is my first ever Machine Learning project outside of school
+
 import time
 import os
 import psutil
@@ -26,7 +29,7 @@ def load_images_from_folder(folder:str, data:list):
   print("Folder: " + folder)
 
   for image in list_of_files:
-    if(image[-4:] == "jpeg"):
+    if(image[-3:] == "png" or image[-4:] == "jpeg"):
       digital_image = img.open(folder+'/'+image).convert("RGB")
       matrix_convert = np.asarray(digital_image)
       data.append(matrix_convert)
@@ -82,45 +85,35 @@ if __name__ == "__main__":
 
   os.system("clear")
 
-  encoding                  = True
+  #Runtime parameters. This is my own "jupyter" Although, I would port the code soon to a notebook.
   training_A                = True
   training_A_dump           = True
-  training_B                = False
-  load_training_A           = False
-  training_B_dump           = False
+  training_B                = True
+  load_training_A           = True
+  training_B_dump           = True
   already_trained_model     = False
   load_training_A           = False
   load_training_B           = False
 
   image_size                = 64
 
-  if(encoding == True):
+  #Trains the Shared Encoder + Decoder A
+  if(training_A == True):
+
+    #This part loads the training data
     print("Encoding Phase")
     path_to_data_a = "../data/without_glasses/tim/extract/size_change_64px"
     data_a = []
     data_a = load_images_from_folder(path_to_data_a, data_a)
 
-
-    path_to_data_b = "../data/without_glasses/harrypotter/extract/size_change_64px"
-    data_b = []
-    data_b = load_images_from_folder(path_to_data_b, data_b)
-
-    if(training_A):
-      training_data = data_a
-    if(training_B):
-      training_data = data_b
-
-    print(len(training_data))
-
-  if(training_A == True):
+    #Prepares and instantiates the autoencoder model
     x = Input(shape=(64,64,3))
     my_encoder = Encoder()
     my_decoder_A = Decoder()
-    optimizer = Adam(lr=5e-5, beta_1=0.5, beta_2=0.999)
-
     auto_encoder = Model(x, my_decoder_A(my_encoder(x)))
 
-    #MODEL Instantiation : https://blog.keras.io/building-autoencoders-in-keras.html
+    #Sets the paramters for the optimizer and the loss function
+    optimizer = Adam(lr=5e-5, beta_1=0.5, beta_2=0.999)
     auto_encoder.compile(optimizer=optimizer, loss='mean_absolute_error')
 
     #Stats of the network https://laid.delanover.com/debugging-a-keras-neural-network/
@@ -131,82 +124,98 @@ if __name__ == "__main__":
     for layer in my_decoder_A.layers:
       print("Input shape: "+str(layer.input_shape)+". Output shape: "+str(layer.output_shape))
     print("Autoencoder##############")
-    # for layer in auto_encoder.layers:
-    #   print("Input shape: "+str(layer.input_shape)+". Output shape: "+str(layer.output_shape))
+
     print(my_encoder.summary())
     print(my_decoder_A.summary())
     print(auto_encoder.summary())
 
     print("\n\n\nTraining Phase")
-    norm_training_data = [x/255 for x in training_data] #normalizing the data
-    x_train = np.reshape(norm_training_data, (len(training_data), image_size, image_size, 3))
-    auto_encoder.fit(x_train,
-                    x_train,
-                    # epochs=1,
-                    epochs=50,
-                    batch_size=32,
-                    shuffle=True,
-                    validation_split=0.25)
+    norm_training_data = [x/255 for x in data_a] #normalize the data so it will converge faster
+    x_train = np.reshape(norm_training_data, (len(data_a), image_size, image_size, 3))
+    auto_encoder.fit( x_train,                    #Set the other training parameters and begin
+                      x_train,
+                      # epochs=1,
+                      epochs=100,
+                      batch_size=32,
+                      shuffle=True,
+                      validation_split=0.25 )
 
+    #Dumping the weights
     if(training_A_dump == True):
-      my_encoder.save_weights('weights_encoder.h5')
-      my_decoder_A.save_weights('weights_decoder_A.h5')
+      my_encoder.save_weights('weights/7564ef9/weights_encoder.h5')
+      my_decoder_A.save_weights('weights/7564ef9/weights_decoder_A.h5')
 
+
+
+  #Trains the Shared Encoder + Decoder B
   if(training_B == True):
+    #This part loads the training data
+    print("Encoding Phase")
+    path_to_data_b = "../data/without_glasses/harrypotter/extract/size_change_64px"
+    data_b = []
+    data_b = load_images_from_folder(path_to_data_b, data_b)
 
+    #Prepares and instantiates the autoencoder model
     x = Input(shape=(64,64,3))
     my_encoder = Encoder()
     my_decoder_B = Decoder()
-    optimizer = Adam(lr=5e-5, beta_1=0.5, beta_2=0.999)
-
     auto_encoder = Model(x, my_decoder_B(my_encoder(x)))
-    #MODEL Instantiation : https://blog.keras.io/building-autoencoders-in-keras.html
+
+    #Sets the paramters for the optimizer and the loss function
+    optimizer = Adam(lr=5e-5, beta_1=0.5, beta_2=0.999)
     auto_encoder.compile(optimizer=optimizer, loss='mean_absolute_error')
 
+    #Since this is a shared encoder, we have to load the weights trained in the first stage with decoder A
     if(load_training_A == True):
-      my_encoder.load_weights('weights_encoder.h5')
+      my_encoder.load_weights('weights/7564ef9/weights_encoder.h5')
 
     print("Training Phase")
-    norm_training_data = [x/255 for x in training_data] #normalizing the data
-    x_train = np.reshape(norm_training_data, (len(training_data), image_size, image_size, 3))
-    auto_encoder.fit(x_train,
-                    x_train,
-                    # epochs=1,
-                    epochs=50,
-                    batch_size=32,
-                    shuffle=True,
-                    validation_split=0.25)
+    norm_training_data = [x/255 for x in data_a] #normalize the data so it will converge faster
+    x_train = np.reshape(norm_training_data, (len(data_a), image_size, image_size, 3))
+    auto_encoder.fit( x_train,                    #Set the other training parameters and begin
+                      x_train,
+                      # epochs=1,
+                      epochs=100,
+                      batch_size=32,
+                      shuffle=True,
+                      validation_split=0.25 )
 
+    #Dumping the weights
     if(training_B_dump == True):
       my_encoder.save_weights('weights_encoder.h5')
       my_decoder_B.save_weights('weights_decoder_B.h5')
 
-
+  #Assumes that this model has been trained before, and weights can be loaded during execution
   if(already_trained_model == True):
 
+    #Prepares and instantiates the autoencoder model
     x = Input(shape=(64,64,3))
     my_encoder = Encoder()
     my_decoder_A = Decoder()
     my_decoder_B = Decoder()
-    optimizer = Adam(lr=5e-5, beta_1=0.5, beta_2=0.999)
-
+    #This uses the shared encoder + the decoder B to put Harry's face on mine.
     auto_encoder = Model(x, my_decoder_B(my_encoder(x)))
-    #MODEL Instantiation : https://blog.keras.io/building-autoencoders-in-keras.html
+
+    #Sets the paramters for the optimizer and the loss function
+    optimizer = Adam(lr=5e-5, beta_1=0.5, beta_2=0.999)
     auto_encoder.compile(optimizer=optimizer, loss='mean_absolute_error')
 
+    #For printing the model stats
     print(auto_encoder.layers)
     print(auto_encoder.summary())
 
     for layer in auto_encoder.layers:
       print("Input shape: "+str(layer.input_shape)+". Output shape: "+str(layer.output_shape))
 
-
+    #To load the weights for putting my face on Harry's face
     if(load_training_A == True):
       my_encoder.load_weights('weights_encoder.h5')
       my_decoder_A.load_weights('weights_decoder_A.h5')
+
+    #To load the weights for putting Harry's face on mine
     if(load_training_B == True):
-      my_encoder.load_weights('weights/24b997c/weights_encoder.h5')
-      my_decoder_B.load_weights('weights/24b997c/weights_decoder_B.h5')
+      my_encoder.load_weights('weights/7564ef9/weights_encoder.h5')
+      my_decoder_B.load_weights('weights/7564ef9/weights_decoder_B.h5')
 
 
     #Let's get to frame rendering now :D
@@ -229,6 +238,7 @@ if __name__ == "__main__":
       r = r + 50
       b = b + 150
 
+      #To calculate the size of the cropped image, so that reconstruction will reshape the swapped faces to original size.
       orig_width = r - l
       orig_length = b - t
 
